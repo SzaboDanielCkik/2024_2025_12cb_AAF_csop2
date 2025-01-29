1818aa0a640754cb388e24a332cd723c324f0ad3
Szabó Dániel <szabo.daniel@ckik.loc>
2025-01-29 11:31:18 +0100

    2025_01_29

--- 2025_01_16 Mukorcsolya/2025_01_16 Mukorcsolya/Program.cs
+++ 2025_01_16 Mukorcsolya/2025_01_16 Mukorcsolya/Program.cs
@@ -16,12 +16,69 @@ namespace _2025_01_16_Mukorcsolya
             donto = Fajlbeolvasas("donto.csv");
             rovidprogram = Fajlbeolvasas("rovidprogram.csv");
 
+            Console.WriteLine(OsszpontSzam("Xiangning LI"));
+
             Feladat2();
             Feladat3();
             Feladat5_6();
-           
+            Feladat7();
+            // Írassa ki a legjobb versenyzőket országonként (ahol 1-nél többb versenyző volt)
+            Feladat7_1();
+            Feladat8();
 
             Console.ReadLine();
+        }
+
+        private static void Feladat8()
+        {
+            StreamWriter sw = new StreamWriter("vegeredmeny.csv");
+            var csoportositas = rovidprogram.Select(x => new
+            {
+                nev = x.nev,
+                orszag = x.orszag,
+                osszpont = OsszpontSzam(x.nev)
+            })
+                .OrderByDescending(x=>x.osszpont);
+            int i = 1;
+            foreach (var item in csoportositas)
+            {
+                sw.WriteLine($"{i};{item.nev};{item.orszag};{item.osszpont} ");
+                i++;
+            }
+            sw.Close();
+        }
+
+        private static void Feladat7_1()
+        {
+            var csoportositas = donto.GroupBy(versenyzo => versenyzo.orszag)
+                .Where(obj => obj.Count() > 1)
+                .Select(obj => new
+                {
+                    orszagkod = obj.Key,
+                    legjobb = obj.ToList().Find(c => c.Osszpont == obj.Max(x => x.Osszpont))
+                });
+                
+
+            foreach (var item in csoportositas)
+            {
+                Console.WriteLine($"{item.orszagkod} - {item.legjobb.nev}: {item.legjobb.Osszpont} ");
+            }
+        }
+
+        private static void Feladat7()
+        {
+            var csoportositas = donto.GroupBy(versenyzo => versenyzo.orszag)
+                .Select(obj => new
+                {
+                    orszagkod = obj.Key,
+                    darab = obj.Count()
+                }).
+                Where(x=>x.darab>1);
+
+            foreach (var item in csoportositas)
+            {
+                Console.WriteLine($"{item.orszagkod} {item.darab}");
+            }
         }
 
         private static void Feladat5_6()
